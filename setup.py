2b0504f680dd98bdf988087c71014171256dc34c
QRCODE <1066756936@qq.com>
2026-01-11 11:19:55 +0800

    tiny bug fixed
    Signed-off-by: QRCODE <1066756936@qq.com>

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
 from os import makedirs, path
 
-makedirs('files\\public', exist_ok=True)
-makedirs('groups', exist_ok=True)
-makedirs('userData', exist_ok=True)
-makedirs('votes', exist_ok=True)
-open('groups\\default', 'w').write(''''created_date':'2025-08-13',
+dirs = ['files\\public', 'groups', 'userData', 'votes']
+default = {
+    'groups\\default': ''''created_date':'2025-08-13',
 'members':[],
-'pwd':'',''')
-open('groups\\public.dat', 'w').write(''''created_date':'2025-08-13',
+'pwd':'',''',
+    'groups\\public.dat': ''''created_date':'2025-08-13',
 'members':'all',
-'pwd':'',''')
-open('groups\\public.grp', 'w').write('')
-open('userData\\default', 'w').write(''''username':'guest',
+'pwd':'',''',
+    'groups\\public.grp': '',
+    'userData\\default': ''''username':'guest',
 'pwd':'',
 'user_type':'guest',
 'remember_me':False,
 'show_uid':True,
-'create_group':0,''')
-open('votes\\default', 'w').write(''''title':'',
+'create_group':0,''',
+    'votes\\default': ''''title':'',
 'start_time':0,
 'For':[],
 'against':[],
-'last_result':[],''')
+'last_result':[],''',
+    'votes\\public.vot': ''''title':'',
+'start_time':0,
+'For':[],
+'against':[],
+'last_result':[],''',
+}
+
+for d in dirs:
+    makedirs(d, exist_ok=True)
+for file, content in default:
+    if not path.exists(file):
+        open(file, 'w').write(content)
 if not path.exists('encrypt.py'):
     open('encrypt.py', 'w').write('''def encrypt(orig, ext):
     return orig+ext
